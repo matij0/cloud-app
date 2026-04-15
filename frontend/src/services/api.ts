83c648945a175d4d3ac8e76272535c1c937e5d52
matij0 <m-damian10@wp.pl>
2026-04-15 21:25:30 +0200

    Koniec

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
+  baseURL: 'https://cloud-task-manager-api-96687-dcffgwezeggsgrh8.polandcentral-01.azurewebsites.net/api'
 });
 
 export default api;
